e278db48ebf3bf1f58feafb9e9a2a7f90ad7bcb6
anh123667877 <rayanshahinhuaweigr5@gmail.com>
2025-02-27 17:28:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
+// add MALVIN-XD~Bhx3mJjY#6WzX4S_Wz7ZUh1jS1-FZGC0QRvcm54M5_A8RvQMsmwk Session Id 
 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
